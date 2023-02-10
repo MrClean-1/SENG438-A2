2713b1b178a33258c69e2d1b074edfd8fdaeb4be
Clean <cjdrewes@gmail.com>
2023-02-09 20:35:47 -0700

    Slight final changes to test

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -61,65 +61,56 @@ public class RangeTest {
     //contains
     @Test
     public void containsTestOneInRange() {
-        boolean expected = true;
         boolean actual = exampleRange.contains(1);
-        assertEquals(expected, actual);
+        assertEquals(true, actual);
     }
 
     @Test
     public void containsTestNegOneInRange() {
-        boolean expected = true;
         boolean actual = exampleRange.contains(-1);
-        assertEquals(expected, actual);
+        assertEquals(true, actual);
     }
 
     @Test
     public void containsTestZeroInRange() {
-        boolean expected = true;
         boolean actual = largeRange.contains(0);
-        assertEquals(expected, actual);
+        assertEquals(true, actual);
     }
 
     @Test
     public void containsTestTwoOutOfRange() {
-        boolean expected = false;
         boolean actual = exampleRange.contains(2);
-        assertEquals(expected, actual);
+        assertEquals(false, actual);
     }
 
     @Test
     public void containsTestLargeNegOutOfRange() {
-        boolean expected = false;
         boolean actual = largeRange.contains(-99999);
-        assertEquals(expected, actual);
+        assertEquals(false, actual);
     }
 
     @Test
     public void containsTestLargePosOutOfRange() {
-        boolean expected = false;
         boolean actual = largeRange.contains(140000);
-        assertEquals(expected, actual);
+        assertEquals(false, actual);
     }
 
     //intersect
     @Test
     public void intersectsTestInRange() {
-        boolean expected = true;
         boolean actual = exampleRange.intersects(-1, 2);
-        assertEquals(expected, actual);
+        assertEquals(true, actual);
     }
 
     @Test
     public void intersectsTestOutOfRangeAbove() {
-        boolean expected = false;
         boolean actual = largeRange.intersects(139995.876, 149995.875);
-        assertEquals(expected, actual);
+        assertEquals(false, actual);
     }
 
     @Test
     public void intersectsTestOutOfRangeBelow() {
-        boolean expected = false;
         boolean actual = largeRange.intersects(-1099995, -99995.876);
-        assertEquals(expected, actual);
+        assertEquals(false, actual);
     }
 }
